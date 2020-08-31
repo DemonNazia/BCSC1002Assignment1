854b02ae3dcaa38288ce4c60584bbdbea31a0c59
Nazia Farheen <nazia.farheen_cs19@gla.ac.in>
2020-08-30 20:40:49 -1200

    adding constructor for field in Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -10,4 +10,17 @@ public class Library {
 
     public static final int MAX_BOOKS_IN_LIBRARY = 10;
     public Book[] currentlyAvailableBooks;
+
+
+    public Library() {
+        this.currentlyAvailableBooks = new Book[MAX_BOOKS_IN_LIBRARY];
+        for (int libraryIndex = 0; libraryIndex < MAX_BOOKS_IN_LIBRARY; libraryIndex++) {
+            this.currentlyAvailableBooks[libraryIndex] = new Book();
+        }
+    }
+
+
+    public Library(Book[] currentlyAvailableBooks) {
+        this.currentlyAvailableBooks = currentlyAvailableBooks;
+    }
 }
